fd12adcebb5d7066e53436fee71cf47e0978ab3a
SimeonNedyalkov <simeon_sg@abv.bg>
2024-12-09 13:35:45 +0200

    smoother animations

--- clothing-store/src/components/Home.tsx
+++ clothing-store/src/components/Home.tsx
@@ -4,6 +4,7 @@ import Cloth from "../types/Cloth";
 import lenionLogo from "../assets/logo/cosmos.png";
 export default function Home(props: { allClothes: Cloth[] }) {
   const [isWebsiteVisible, setIsWebsiteVisible] = useState(false);
+  const [isWebsiteVisibleToggle, setIsWebsiteVisibleToggle] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
@@ -11,11 +12,25 @@ export default function Home(props: { allClothes: Cloth[] }) {
       setIsWebsiteVisible(true);
     }, 2000);
   }, []);
+  useEffect(() => {
+    const timeoutDelay = window.innerWidth <= 768 ? 2800 : 2600;
+    setTimeout(() => {
+      setIsWebsiteVisibleToggle(false);
+      setIsWebsiteVisibleToggle(true);
+    }, timeoutDelay);
+  }, []);
 
   return (
     <div className={`website-content ${isWebsiteVisible ? "visible" : "home"}`}>
       <div className="image-container">
-        <img src={lenionLogo} alt="" className="background-image" />
+        <img
+          src={lenionLogo}
+          alt=""
+          className={`background-image ${
+            isWebsiteVisibleToggle ? "visibleImage" : "hiddenImage"
+          }`}
+        />
+
         <div className="homeSliderContainer">
           <div className="pointed-boxLeft"></div>
           <Slider allClothes={props.allClothes} />
@@ -26,13 +41,18 @@ export default function Home(props: { allClothes: Cloth[] }) {
         <span>COSMOS</span>
         <span>COLLECTION</span>
       </div>
+
       <div className="toggleButtonContainer justify-items-end content-end items-end">
         <div className="newCollectionContainer flex flex-col justify-items-end content-end items-end">
           <span className="">Newest collection on LENION</span>
           <span className="">with integrated AI</span>
           <div className="flex flex-row mt-2">
             <input type="checkbox" id="switch" />
-            <label htmlFor="switch">Toggle</label>
+            {isWebsiteVisibleToggle ? (
+              <label htmlFor="switch">Toggle</label>
+            ) : (
+              <div></div>
+            )}
             <span className="addToCart">Add to cart</span>
           </div>
         </div>
