6dfbdb5d60660236b4c1726d5434819a78cd2825
SimeonNedyalkov <simeon_sg@abv.bg>
2024-12-09 11:50:37 +0200

    homepage added texts

--- clothing-store/src/components/Home.tsx
+++ clothing-store/src/components/Home.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Slider from "./Slider";
 import Cloth from "../types/Cloth";
+import lenionLogo from "../assets/logo/cosmos.png";
 export default function Home(props: { allClothes: Cloth[] }) {
   const [isWebsiteVisible, setIsWebsiteVisible] = useState(false);
 
@@ -13,10 +14,28 @@ export default function Home(props: { allClothes: Cloth[] }) {
 
   return (
     <div className={`website-content ${isWebsiteVisible ? "visible" : "home"}`}>
-      <div className="homeSliderContainer">
-        <div className="pointed-boxLeft"></div>
-        <Slider allClothes={props.allClothes} />
-        <div className="pointed-boxRight"></div>
+      <div className="image-container">
+        <img src={lenionLogo} alt="" className="background-image" />
+        <div className="homeSliderContainer">
+          <div className="pointed-boxLeft"></div>
+          <Slider allClothes={props.allClothes} />
+          <div className="pointed-boxRight"></div>
+        </div>
+      </div>
+      <div className="cosmosCollection">
+        <span>COSMOS</span>
+        <span>COLLECTION</span>
+      </div>
+      <div className="toggleButtonContainer justify-items-end content-end items-end">
+        <div className="newCollectionContainer flex flex-col justify-items-end content-end items-end">
+          <span className="">Newest collection on LENION</span>
+          <span className="">with integrated AI</span>
+          <div className="flex flex-row mt-2">
+            <input type="checkbox" id="switch" />
+            <label htmlFor="switch">Toggle</label>
+            <span className="addToCart">Add to cart</span>
+          </div>
+        </div>
       </div>
     </div>
   );
