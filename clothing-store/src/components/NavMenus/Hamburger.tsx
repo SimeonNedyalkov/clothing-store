bd4a21e6e63b4d63816e2e3c0a130646c2300220
SimeonNedyalkov <simeon_sg@abv.bg>
2024-12-20 00:08:09 +0200

    burger menu fix cart fix

--- clothing-store/src/components/NavMenus/Hamburger.tsx
+++ clothing-store/src/components/NavMenus/Hamburger.tsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 import Cloth from "../../types/Cloth";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { toggleCart } from "../reduxstore/store";
+import { toggleCart, useTypedSelector } from "../reduxstore/store";
+import Cart from "../Cart";
 
 export default function Hamburger(props: { allClothes: Cloth[] }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [categories, setCategories] = useState<string[]>([]);
-
+  const isCartOpen = useTypedSelector((state) => state.cart.isCartOpen);
   const dispatch = useDispatch();
   const handleCartClick = (e: any) => {
     e.preventDefault();
@@ -30,93 +31,106 @@ export default function Hamburger(props: { allClothes: Cloth[] }) {
   }
   return (
     <>
-      <div className="hamburger-menu">
-        <div
-          id="hamburger"
-          className={`hamburglar ${isMenuOpen ? "is-open" : "is-closed"}`}
-          onClick={handleClick}
-        >
-          <div className="burger-icon">
-            <div className="burger-container">
-              <span className="burger-bun-top"></span>
-              <span className="burger-filling"></span>
-              <span className="burger-bun-bot"></span>
-            </div>
-          </div>
-
-          {/* svg ring container */}
-          <div className="burger-ring">
-            <svg className="svg-ring">
-              <path
-                className="path"
-                fill="none"
-                stroke="#fff"
-                strokeMiterlimit="10"
-                strokeWidth="4"
-                d="M 34 2 C 16.3 2 2 16.3 2 34 s 14.3 32 32 32 s 32 -14.3 32 -32 S 51.7 2 34 2"
-              />
-            </svg>
-          </div>
-
-          {/* The masked path that animates the fill to the ring */}
-          <svg width="0" height="0">
-            <mask id="mask">
-              <path
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                stroke="#ff0000"
-                strokeMiterlimit="10"
-                strokeWidth="4"
-                d="M 34 2 c 11.6 0 21.8 6.2 27.4 15.5 c 2.9 4.8 5 16.5 -9.4 16.5 h -4"
-              />
-            </mask>
-          </svg>
-
-          <div className="path-burger">
-            <div className="animate-path">
-              <div className="path-rotation"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      // {/* Navigation Links Section */}
-      <div className={`nav-links ${isMenuOpen ? "open" : ""}`}>
-        <div className="category-dropdown">
-          <a href="#category" className="category-link">
-            Category{" "}
-            <svg
-              className="icon icon-caret"
-              xmlns="http://www.w3.org/2000/svg"
-              width="7.55"
-              height="4.77"
-              viewBox="0 0 6.257 3.835"
+      {!isCartOpen ? (
+        <>
+          <div className="hamburger-menu">
+            <div
+              id="hamburger"
+              className={`hamburglar ${isMenuOpen ? "is-open" : "is-closed"}`}
+              onClick={handleClick}
             >
-              <path
-                data-name="Path 12050"
-                d="M659.672,95,656.9,97.775,654.122,95"
-                transform="translate(-653.769 -94.646)"
-                fill="none"
-                stroke="#222"
-                strokeWidth="1"
-              ></path>
-            </svg>
-          </a>
-          <div className="dropdown-content">
-            {categories.map((category, index) => (
-              <a href={`#${category}`} key={index}>
-                {category}
-              </a>
-            ))}
+              <div className="burger-icon">
+                <div className="burger-container">
+                  <span className="burger-bun-top"></span>
+                  <span className="burger-filling"></span>
+                  <span className="burger-bun-bot"></span>
+                </div>
+              </div>
+
+              {/* svg ring container */}
+              <div className="burger-ring">
+                <svg className="svg-ring">
+                  <path
+                    className="path"
+                    fill="none"
+                    stroke="#fff"
+                    strokeMiterlimit="10"
+                    strokeWidth="4"
+                    d="M 34 2 C 16.3 2 2 16.3 2 34 s 14.3 32 32 32 s 32 -14.3 32 -32 S 51.7 2 34 2"
+                  />
+                </svg>
+              </div>
+
+              {/* The masked path that animates the fill to the ring */}
+              <svg width="0" height="0">
+                <mask id="mask">
+                  <path
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    stroke="#ff0000"
+                    strokeMiterlimit="10"
+                    strokeWidth="4"
+                    d="M 34 2 c 11.6 0 21.8 6.2 27.4 15.5 c 2.9 4.8 5 16.5 -9.4 16.5 h -4"
+                  />
+                </mask>
+              </svg>
+
+              <div className="path-burger">
+                <div className="animate-path">
+                  <div className="path-rotation"></div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        <a href="#brand">Brand</a>
-        <a href="#ranking">Ranking</a>
-        <Link onClick={handleCartClick} to="#">
-          Cart
-        </Link>
-        <a href="#about">About</a>
-        <a href="#qa">Q&A</a>
-      </div>
+          // {/* Navigation Links Section */}
+          <div className={`nav-links ${isMenuOpen ? "open" : ""}`}>
+            <div className="category-dropdown">
+              <a href="#category" className="category-link">
+                Category{" "}
+                <svg
+                  className="icon icon-caret"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="8"
+                  height="5"
+                  viewBox="0 0 6.257 3.835"
+                >
+                  <path
+                    d="M659.672,95,656.9,97.775,654.122,95"
+                    transform="translate(-653.769 -94.646)"
+                    fill="none"
+                    stroke="#fff"
+                    strokeWidth="1.2"
+                  ></path>
+                </svg>
+              </a>
+              <div className="dropdown-content">
+                {categories.map((category, index) => (
+                  <a href={`#${category}`} key={index}>
+                    {category}
+                  </a>
+                ))}
+              </div>
+            </div>
+            <a href="#brand" className="nav-link">
+              Brand
+            </a>
+            <a href="#ranking" className="nav-link">
+              Ranking
+            </a>
+            <Link onClick={handleCartClick} to="#" className="nav-link">
+              Cart
+            </Link>
+            <a href="#about" className="nav-link">
+              About
+            </a>
+            <a href="#qa" className="nav-link">
+              Q&A
+            </a>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
